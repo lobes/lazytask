2438b7e47d14fae4e521dd6ffd31de79910cfa60
lobes <samuel.t.giles@gmail.com>
2024-04-16 07:02:07 +1000

    rename task status to task state

--- pkg/commands/models/task.go
+++ pkg/commands/models/task.go
@@ -31,7 +31,7 @@ type Task struct {
 	Description string
 	Tags        []string
 	Priority    Priority
-	Status      string
+	State       string
 	DueOn       time.Time
 	Parents     []string
 }
@@ -40,46 +40,39 @@ func (t Task) Error() string {
 	return fmt.Sprintf(`
     "Task Error: [%s] %s 
     Priority: %s
-    Status: %s`, t.Uuid, t.Description, t.Priority, t.Status)
+    State: %s`, t.Uuid, t.Description, t.Priority, t.State)
 
 }
 
 func ReadTask() {
-	var testicles Task
-	if _, err := toml.DecodeFile("sisiphus/tasks/template.toml", &testicles); err != nil {
+	var fromDisk Task
+	if _, err := toml.DecodeFile("sisiphus/tasks/template.toml", &fromDisk); err != nil {
 		fmt.Printf("Error decoding task from toml: %s\n", err)
 	}
-	fmt.Printf("description: %s\n", testicles.Description)
+	fmt.Printf("description: %s\n", fromDisk.Description)
 }
 
-func (t *Task) SetStatus(newStatus string) error {
-	// Here, you could enforce rules. For example:
-	// - Disallow moving away from StatusDone
-	// - Define valid transitions if needed
-	if t.Status == Done && newStatus != Done {
-		return fmt.Errorf("invalid transition from %s to %s", Done, newStatus)
+func (t *Task) SetState(newState string) error {
+	// TODO: expand to the whole task state machine
+	if t.State == Done && newState != Done {
+		return fmt.Errorf("invalid transition from %s to %s", Done, newState)
 	}
-	t.Status = newStatus
+	t.State = newState
 	return nil
 }
 
 func (t Task) CreateTask(data Task) error {
+	// encodes it as <Uuid>.toml
+	// err if a file with that name already exists
 	return Task{
 		Uuid:        uuid.NewString(),
 		Description: data.Description,
 	}
 }
 
-// func createTask()
+// func UpdateTask()
 // is passed in a Task
-// generates a uuid
-// assigns it to the field uuid
-// encodes it as <uuid>.toml
-// err if a file with that name already exists
+// encodes it as <Uuid>.toml and over-writes any existing file if there
 
-// func updateTask()
-// is passed in a Task
-// encodes it as <uuid>.toml and over-writes any existing file if there
-
-// func deleteTask()
+// func DeleteTask()
 // is passed in a Uuid
